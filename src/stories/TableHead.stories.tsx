6bb395f861b09e85d114d5148f0be11b112f8467
OLUSOLA <olusola@OLUSOLAs-MacBook-Pro.local>
2022-08-05 10:05:59 +0100

    Implement table head

---
+++ src/stories/TableHead.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Box, IconButton, Typography, Card } from '@mui/material'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+import TableHead from '../components/table/TableHead';
+import ThemeProviderWrapper from './ThemeProviderWrapper';
+
+export default {
+    title: "SterlingCore/Table/Head",
+    component: TableHead,
+    argTypes: {},
+} as ComponentMeta<typeof TableHead>;
+
+const SeeAllActionButton = () => (
+    <Box sx={{ textTransform: "none" }}>
+        <IconButton sx={{ bgcolor: "primary.light", borderRadius: 1 }} >
+            <VisibilityIcon fontSize="small" color="primary" />
+        </IconButton>
+        <Typography variant="body2" color="primary">See all</Typography>
+    </Box>
+);
+
+const rowHead = [
+    { id: 'role', label: 'Role', align: 'left' },
+    { id: 'updated', label: 'Updated On', align: 'left' },
+    { id: 'status', label: 'Status', align: 'left' },
+    { id: 'description', label: 'Description', align: 'left' },
+    { id: 'action', label: <SeeAllActionButton />, align: 'center' }
+];
+
+const Template: ComponentStory<typeof TableRow> = (args) => (
+    <ThemeProviderWrapper>
+        <Card>
+            <TableHead {...args} />
+        </Card>
+    </ThemeProviderWrapper>
+);
+
+export const tableRow = Template.bind({});
+tableRow.args = {
+    order: "asc",
+    orderBy: "name",
+    headLabel: rowHead
+};
